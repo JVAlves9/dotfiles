eb17f0aace05ee99b93f0b3ea95f1aafb506c0dd
JVAlves9 <d-jvictor@hotmail.com>
2026-02-25 13:35:37 -0300

    add: none-ls config with json 4 spaces indent

---
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -0,0 +1,14 @@
+return {
+  "nvimtools/none-ls.nvim",
+  opts = function(_, opts)
+    local nls = require("null-ls")
+    opts.sources = opts.sources or {}
+
+    table.insert(
+      opts.sources,
+      nls.builtins.formatting.prettier.with({
+        extra_args = { "--tab-width", "4" },
+      })
+    )
+  end,
+}
